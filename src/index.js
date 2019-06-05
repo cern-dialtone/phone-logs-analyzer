2b8df55aafedf49e21b4e5d29091f33c83999083
Lorys Lopez <loryslopez@imac02.dyndns.cern.ch>
2019-06-05 10:31:45 +0200

    deleting useless files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Form, TextArea } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const UploadForm = (props) => {
+    return (<Form className="upload" onChange={props.onChange}>
+        <input type="file"/>
+      <TextArea placeholder='Paste logs' style={{marginTop: 50}}/>
+    </Form>);
+}
+export default UploadForm
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Panel extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {screen:'upload'};
+    }
+
+    render() {
+            return <UploadForm onChange={() => this.loadlogs()}/>;
+    }
+}
+
+class Body extends React.Component {
+    render() {
+        return <div>
+            <h1 className="title">Logs analyzer</h1>
+            <Panel />
+            </div>;
+    }
+}
+
+ReactDOM.render(<Body />, document.getElementById('root'));
