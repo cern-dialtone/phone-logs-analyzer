cfc9ca645521395b23b1b57efd3a4692cbcd3a5e
lopez <llopez@student.42.fr>
2019-07-17 16:26:15 +0200

    nearly finished parsing

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -26,7 +26,72 @@ export class App extends React.Component {
   loadJson(e) {
     this.setState({ logs: e });
   }
+
+  split_actions(str) {
+    let next = [];
+    let cut = 0;
+    let tmp = "";
+
+    for (let i=0; str.charAt(i); i++)
+    {
+      if (str.charAt(i) === '\n' && str.charAt(i) === '\n' && str.charAt(i+1) !== ' ')
+        cut = 1;
+      if (cut) {
+        next.push(tmp);
+        tmp = "";
+        cut = 0;
+      }
+      else
+        tmp += str.charAt(i);
+    }
+    return (next);
+  }
+
+  split_data(array) {
+    let next = [];
+    let cut = 0;
+    let tmp = "";
+    let count = 0;
+
+    for (let a = 0; a < array.length; a++) {
+      next[a] = [];
+      count = 0;
+      for (let i=0; array[a].charAt(i); i++)
+      {
+        if (array[a].substr(i, 3) === ' | ' && count < 2)
+        {
+          i += 2;
+          count++;
+          next[a].push(tmp);
+          tmp = "";
+        }
+        else
+          tmp += array[a].charAt(i);
+      }
+      next[a].push(tmp);
+    }
+    return (next);
+  }
+
+  convertToJson(e) {
+    let tmp;
+    let next = [];
+
+    tmp = this.split_actions(e);
+    tmp = this.split_data(tmp);
+    for (let a = 0; a < tmp.length; a++){
+
+      tmp[a][2] = tmp[a][2].split('\'');
+      tmp[a][2] = tmp[a][2].join('"');
+      tmp[a][2] = tmp[a][2].split('undefined');
+      tmp[a][2] = tmp[a][2].join('""');
+    console.log(tmp[a][2]);
+      tmp[a][2] = JSON.parse("["+tmp[a][2]+"]");}
+    console.log(tmp);
+  }
+
   changePanel(e) {
+    e = this.convertToJson(e);
     if (isJson(e)) {
       document.getElementById('interpret').classList.toggle('active');
       document.getElementById('home').classList.toggle('active');
