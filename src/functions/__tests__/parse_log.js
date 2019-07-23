86ce745a6bd82b75bd0a083c76034cc27abab14e
Lorys <31002428+lorys@users.noreply.github.com>
2019-07-23 13:45:17 +0200

    Update parse_log.js

--- src/functions/__tests__/parse_log.js
+++ src/functions/__tests__/parse_log.js
@@ -2,13 +2,6 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react'
 import { getType, getInfos, getTime, getColor } from '../parse_log'
 
-it('getInfos works', () => {
-    expect(getInfos(["i18next::backendConnector: loaded namespace translations for language en"])).toBe(" loaded namespace translations for language en");
-    expect(getInfos(["i18next: initialized"])).toBe(" initialized");
-    expect(getInfos(["%cAPP:INFO %cLogin user with code...%c +0ms"])).toBe("Login user with code...");
-    expect(getInfos(["Fri May 31 2019 14:48:45 GMT+0200 (Central European Summer Time) | sip.ua | configuration parameters after validation:"])).toBe(" configuration parameters after validation:");
-});
-
 it('getColor works', () => {
     expect(getColor(["color: #4286f4"])).toBe("rgba(66, 134, 244, 0.3)");
     expect(getColor(["color: #428"])).toBe("transparent");
