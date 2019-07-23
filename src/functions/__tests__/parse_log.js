b154b930a364f54e4a7cd4befef30fda5c8ec7a7
Lorys <31002428+lorys@users.noreply.github.com>
2019-07-23 13:25:29 +0200

    Change tests

--- src/functions/__tests__/parse_log.js
+++ src/functions/__tests__/parse_log.js
@@ -2,13 +2,6 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react'
 import { getType, getInfos, getTime, getColor } from '../parse_log'
 
-it('getType works', () => {
-    expect(getType(["i18next::backendConnector: loaded namespace translations for language en"])).toBe("i18next::backendConnector");
-    expect(getType(["i18next: initialized"])).toBe("i18next");
-    expect(getType(["%cAPP:INFO %cLogin user with code...%c +0ms"])).toBe("APP:INFO ");
-    expect(getType(["Fri May 31 2019 14:48:45 GMT+0200 (Central European Summer Time) | sip.ua | configuration parameters after validation:"])).toBe(" sip.ua ");
-});
-
 it('getInfos works', () => {
     expect(getInfos(["i18next::backendConnector: loaded namespace translations for language en"])).toBe(" loaded namespace translations for language en");
     expect(getInfos(["i18next: initialized"])).toBe(" initialized");
